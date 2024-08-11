d2d95f119b97a55fd8f58e980efaf68cc2a8851c
MrShaaciye <43672604+MrShaaciye@users.noreply.github.com>
2024-08-11 13:47:30 +0300

    commit

--- client/src/pages/customers/ViewCustomer.jsx
+++ client/src/pages/customers/ViewCustomer.jsx
@@ -9,7 +9,7 @@ const Button = lazy(() => import("../../components/formsUI/ButtonWrapper"));
 const ViewCustomer = ({ customer, closeEvent }) => {
     return (
         <>
-            {/* Add your form here shaaciye */}
+            {/* Add your form here */}
             <Typography variant="h6" fontWeight="bold" align="center">
                 View Customer
             </Typography>
