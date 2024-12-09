2b68de9ae29304c092ee3bf0c22de9fe6cb29804
MrShaaciye <shaaciye122@gmail.com>
2024-12-09 13:52:47 +0300

    commit

--- tutorial2/server.jsx
+++ tutorial2/server.jsx
@@ -3,6 +3,6 @@ const { v4: uuidv4 } = require(`uuid`);
 
 console.log(format(new Date(), `dd-MM-yyyy HH:mm:ss`));
 console.log(uuidv4());
-const dates = [new Date(`2020-01-01`), new Date(`2021-01-02`), new Date(`2022-01-03`), new Date(`2023-01-04`)];
+const dates = [new Date(`2021-11-15`), new Date(`2022-05-20`), new Date(`2023-08-25`), new Date(`2024-07-11`)];
 
 console.log(dates.sort(compareAsc, `dd-MM-yyyy HH:mm:ss`));
