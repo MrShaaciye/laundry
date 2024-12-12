c8d09761199f9c8218230beb29f45d3a9bc805cf
MrShaaciye <shaaciye122@gmail.com>
2024-12-12 17:02:37 +0300

    commit

--- server/src/middleware/logger.jsx
+++ server/src/middleware/logger.jsx
@@ -2,6 +2,6 @@
 const moment = require("moment");
 
 // Create Logger
-const logger = (req, res, next) => (console.log(`${req.protocol}://${req.get(`host`)}${req.originalUrl} : ${moment().format()}`), next());
+const logger = (req, res, next) => (console.log(`${req.protocol}://${req.get("host")}${req.originalUrl} - ${moment().format()}`), next());
 
 module.exports = logger;
