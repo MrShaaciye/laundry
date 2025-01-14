f0ee2b71146e69cd60ccf2db20c5691c2845dd4a
MrShaaciye <shaaciye122@gmail.com>
2025-01-14 14:31:09 +0300

    commit

--- server/src/controllers/smscustomer.controller.jsx
+++ server/src/controllers/smscustomer.controller.jsx
@@ -2,94 +2,94 @@
 const db = require(`../models/index.jsx`);
 
 // Import smsCustomerModel, Sequelize, Op
-const smsCustomerModel = db.smsCustomerModel;
+const SMSCustomerModel = db.SMSCustomerModel;
 const customerModel = db.customerModel;
 const sequelize = db.sequelize;
 const Op = db.Op;
 
 // Create SMS Customer
 exports.create = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const smsCustomer = await smsCustomerModel.create({ customerId: req.body.customerId, body: req.body.body, transaction: transactions });
-        return await transactions.commit(), res.status(201).json(smsCustomer);
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const smsCustomer = await SMSCustomerModel.create({ customerId: req.body.customerId, body: req.body.body, transaction: transactions });
+    return await transactions.commit(), res.status(201).json(smsCustomer);
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
 
 // Find All SMS Customers
 exports.findAll = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const body = req.body.body;
-        let finder = body ? { body: { [Op.like]: `%${body}%` } } : null;
-        const smsCustomers = await smsCustomerModel.findAndCountAll({
-            attributes: [`id`, `customerId`, `body`, `createdAt`, `updatedAt`, `deletedAt`],
-            include: [{ model: customerModel, as: `customers`, attributes: [`id`, `name`, `gender`, `address`, `phone`, `depositAmount`, `allowedUnit`, `paymentStatus`, `createdAt`, `updatedAt`, `deletedAt`] }],
-            transaction: transactions,
-            lock: false,
-            paranoid: false,
-            order: [[`id`, `DESC`]],
-            where: finder,
-        });
-        return await transactions.commit(), res.status(200).json(smsCustomers);
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const body = req.body.body;
+    let finder = body ? { body: { [Op.like]: `%${body}%` } } : null;
+    const smsCustomers = await SMSCustomerModel.findAndCountAll({
+      attributes: [`id`, `customerId`, `body`, `createdAt`, `updatedAt`, `deletedAt`],
+      include: [{ model: customerModel, as: `customers`, attributes: [`id`, `name`, `gender`, `address`, `phone`, `depositAmount`, `allowedUnit`, `paymentStatus`, `createdAt`, `updatedAt`, `deletedAt`] }],
+      transaction: transactions,
+      lock: false,
+      paranoid: false,
+      order: [[`id`, `DESC`]],
+      where: finder,
+    });
+    return await transactions.commit(), res.status(200).json(smsCustomers);
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
 
 // Find One SMS Customer by id
 exports.findOne = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const id = req.params.id;
-        const smsCustomer = await smsCustomerModel.findOne({
-            attributes: [`id`, `customerId`, `body`, `createdAt`, `updatedAt`, `deletedAt`],
-            include: [{ model: customerModel, as: `customers`, attributes: [`id`, `name`, `gender`, `address`, `phone`, `depositAmount`, `allowedUnit`, `paymentStatus`, `createdAt`, `updatedAt`, `deletedAt`] }],
-            transaction: transactions,
-            lock: false,
-            paranoid: false,
-            where: { id: id },
-        });
-        return smsCustomer ? (await transactions.commit(), res.status(200).json(smsCustomer)) : (await transactions.rollback(), res.status(404).json(`SMS Customer not found`));
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const id = req.params.id;
+    const smsCustomer = await SMSCustomerModel.findOne({
+      attributes: [`id`, `customerId`, `body`, `createdAt`, `updatedAt`, `deletedAt`],
+      include: [{ model: customerModel, as: `customers`, attributes: [`id`, `name`, `gender`, `address`, `phone`, `depositAmount`, `allowedUnit`, `paymentStatus`, `createdAt`, `updatedAt`, `deletedAt`] }],
+      transaction: transactions,
+      lock: false,
+      paranoid: false,
+      where: { id: id },
+    });
+    return smsCustomer ? (await transactions.commit(), res.status(200).json(smsCustomer)) : (await transactions.rollback(), res.status(404).json(`SMS Customer not found`));
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
 
 // Update One SMS Customer by id
 exports.update = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const id = req.params.id;
-        const smsCustomer = await smsCustomerModel.update(req.body, { where: { id: id }, transaction: transactions });
-        return await transactions.commit(), res.status(200).json(smsCustomer);
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const id = req.params.id;
+    const smsCustomer = await SMSCustomerModel.update(req.body, { where: { id: id }, transaction: transactions });
+    return await transactions.commit(), res.status(200).json(smsCustomer);
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
 
 // Restore One SMS Customer by id
 exports.restore = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const id = req.params.id;
-        const smsCustomer = await smsCustomerModel.restore({ where: { id: id }, transaction: transactions });
-        return smsCustomer ? (await transactions.commit(), res.status(200).json(smsCustomer)) : (await transactions.rollback(), res.status(404).json(`SMS Customer not found`));
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const id = req.params.id;
+    const smsCustomer = await SMSCustomerModel.restore({ where: { id: id }, transaction: transactions });
+    return smsCustomer ? (await transactions.commit(), res.status(200).json(smsCustomer)) : (await transactions.rollback(), res.status(404).json(`SMS Customer not found`));
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
 
 // Delete One SMS Customer by id
 exports.delete = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const id = req.params.id;
-        const smsCustomer = await smsCustomerModel.destroy({ where: { id: id }, transaction: transactions });
-        return smsCustomer ? (await transactions.commit(), res.status(200).json(smsCustomer)) : (await transactions.rollback(), res.status(404).json(`SMS Customer not found`));
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const id = req.params.id;
+    const smsCustomer = await SMSCustomerModel.destroy({ where: { id: id }, transaction: transactions });
+    return smsCustomer ? (await transactions.commit(), res.status(200).json(smsCustomer)) : (await transactions.rollback(), res.status(404).json(`SMS Customer not found`));
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
--- server/src/controllers/smsemployee.controller.jsx
+++ server/src/controllers/smsemployee.controller.jsx
@@ -2,94 +2,94 @@
 const db = require(`../models/index.jsx`);
 
 // Import smsEmployeeModel, Sequelize, Op
-const smsEmployeeModel = db.smsEmployeeModel;
+const SMSEmployeeModel = db.SMSEmployeeModel;
 const employeeModel = db.employeeModel;
 const sequelize = db.sequelize;
 const Op = db.Op;
 
 // Create SMS Employee
 exports.create = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const smsEmployee = await smsEmployeeModel.create({ employeeId: req.body.employeeId, body: req.body.body, transaction: transactions });
-        return await transactions.commit(), res.status(201).json(smsEmployee);
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const smsEmployee = await SMSEmployeeModel.create({ employeeId: req.body.employeeId, body: req.body.body, transaction: transactions });
+    return await transactions.commit(), res.status(201).json(smsEmployee);
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
 
 // Find All SMS Employee
 exports.findAll = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const body = req.body.body;
-        let finder = body ? { body: { [Op.like]: `%${body}%` } } : null;
-        const smsEmployees = await smsEmployeeModel.findAndCountAll({
-            attributes: [`id`, `employeeId`, `body`, `createdAt`, `updatedAt`, `deletedAt`],
-            include: [{ model: employeeModel, as: `employees`, attributes: [`id`, `name`, `gender`, `address`, `phone`, `jobTitle`, `salary`, `createdAt`, `updatedAt`, `deletedAt`] }],
-            transaction: transactions,
-            lock: false,
-            paranoid: false,
-            order: [[`id`, `DESC`]],
-            where: finder,
-        });
-        return await transactions.commit(), res.status(200).json(smsEmployees);
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const body = req.body.body;
+    let finder = body ? { body: { [Op.like]: `%${body}%` } } : null;
+    const smsEmployees = await SMSEmployeeModel.findAndCountAll({
+      attributes: [`id`, `employeeId`, `body`, `createdAt`, `updatedAt`, `deletedAt`],
+      include: [{ model: employeeModel, as: `employees`, attributes: [`id`, `name`, `gender`, `address`, `phone`, `jobTitle`, `salary`, `createdAt`, `updatedAt`, `deletedAt`] }],
+      transaction: transactions,
+      lock: false,
+      paranoid: false,
+      order: [[`id`, `DESC`]],
+      where: finder,
+    });
+    return await transactions.commit(), res.status(200).json(smsEmployees);
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
 
 // Find One SMS Employee by id
 exports.findOne = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const id = req.params.id;
-        const smsEmployee = await smsEmployeeModel.findOne({
-            attributes: [`id`, `employeeId`, `body`, `createdAt`, `updatedAt`, `deletedAt`],
-            include: [{ model: employeeModel, as: `employees`, attributes: [`id`, `name`, `gender`, `address`, `phone`, `jobTitle`, `salary`, `createdAt`, `updatedAt`, `deletedAt`] }],
-            transaction: transactions,
-            lock: false,
-            paranoid: false,
-            where: { id: id },
-        });
-        return smsEmployee ? (await transactions.commit(), res.status(200).json(smsEmployee)) : (await transactions.rollback(), res.status(404).json(`SMS Employee not found`));
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const id = req.params.id;
+    const smsEmployee = await SMSEmployeeModel.findOne({
+      attributes: [`id`, `employeeId`, `body`, `createdAt`, `updatedAt`, `deletedAt`],
+      include: [{ model: employeeModel, as: `employees`, attributes: [`id`, `name`, `gender`, `address`, `phone`, `jobTitle`, `salary`, `createdAt`, `updatedAt`, `deletedAt`] }],
+      transaction: transactions,
+      lock: false,
+      paranoid: false,
+      where: { id: id },
+    });
+    return smsEmployee ? (await transactions.commit(), res.status(200).json(smsEmployee)) : (await transactions.rollback(), res.status(404).json(`SMS Employee not found`));
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
 
 // Update One SMS Employee by id
 exports.update = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const id = req.params.id;
-        const smsEmployee = await smsEmployeeModel.update(req.body, { where: { id: id }, transaction: transactions });
-        return await transactions.commit(), res.status(200).json(smsEmployee);
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const id = req.params.id;
+    const smsEmployee = await SMSEmployeeModel.update(req.body, { where: { id: id }, transaction: transactions });
+    return await transactions.commit(), res.status(200).json(smsEmployee);
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
 
 // Restore One SMS Employee by id
 exports.restore = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const id = req.params.id;
-        const smsEmployee = await smsEmployeeModel.restore({ where: { id: id }, transaction: transactions });
-        return smsEmployee ? (await transactions.commit(), res.status(200).json(smsEmployee)) : (await transactions.rollback(), res.status(404).json(`SMS Employee not found`));
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const id = req.params.id;
+    const smsEmployee = await SMSEmployeeModel.restore({ where: { id: id }, transaction: transactions });
+    return smsEmployee ? (await transactions.commit(), res.status(200).json(smsEmployee)) : (await transactions.rollback(), res.status(404).json(`SMS Employee not found`));
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
 
 // Delete One SMS Employee by id
 exports.delete = async (req, res) => {
-    const transactions = await sequelize.transaction();
-    try {
-        const id = req.params.id;
-        const smsEmployee = await smsEmployeeModel.destroy({ where: { id: id }, transaction: transactions });
-        return smsEmployee ? (await transactions.commit(), res.status(200).json(smsEmployee)) : (await transactions.rollback(), res.status(404).json(`SMS Employee not found`));
-    } catch (err) {
-        return await transactions.rollback(), res.status(500).json(err.message);
-    }
+  const transactions = await sequelize.transaction();
+  try {
+    const id = req.params.id;
+    const smsEmployee = await SMSEmployeeModel.destroy({ where: { id: id }, transaction: transactions });
+    return smsEmployee ? (await transactions.commit(), res.status(200).json(smsEmployee)) : (await transactions.rollback(), res.status(404).json(`SMS Employee not found`));
+  } catch (err) {
+    return await transactions.rollback(), res.status(500).json(err.message);
+  }
 };
