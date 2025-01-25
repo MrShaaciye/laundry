8b816bff5c198b245c0331fd6a3394916bb93c78
MrShaaciye <shaaciye122@gmail.com>
2025-01-25 09:26:32 +0300

    Add PageNotFound route for unmatched paths

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -75,6 +75,7 @@ const App = () => {
           ) : (
             <Routes>
               <Route path="/" element={<Login />} />
+              <Route path="*" element={<PageNotFound />} />
             </Routes>
           )}
         </Suspense>
