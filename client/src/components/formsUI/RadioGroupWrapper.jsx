a0a55d08979ea364c59cbe6abeee0b56671d876e
MrShaaciye <shaaciye122@gmail.com>
2025-02-08 10:31:01 +0300

    commit

--- client/src/components/formsUI/RadioGroupWrapper.jsx
+++ client/src/components/formsUI/RadioGroupWrapper.jsx
@@ -4,14 +4,14 @@ import { RadioGroup, FormControlLabel, Radio } from "@mui/material";
 const RadioGroupWrapper = ({ field, form, options, ...props }) => {
   const { name, value, onChange, onBlur } = field;
   const { errors, touched } = form;
-  const fieldError = errors[name] && touched[name];
+  const fieldError = touched[name] && errors[name];
 
   return (
     <RadioGroup {...field} {...props} value={value || ""} onChange={onChange} onBlur={onBlur} row>
       {options.map((option) => (
         <FormControlLabel key={option.value} value={option.value} control={<Radio />} label={option.label} />
       ))}
-      {fieldError && <div style={{ color: "red" }}>{fieldError}</div>}
+      {fieldError && <div style={{ color: "red" }}>{errors[name]}</div>}
     </RadioGroup>
   );
 };
