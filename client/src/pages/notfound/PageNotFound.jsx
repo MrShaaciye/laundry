c42ab95df1b6ed6315cdd8cd10776ba0dbdb0a60
MrShaaciye <shaaciye122@gmail.com>
2025-02-08 10:13:31 +0300

    commit

--- client/src/pages/notfound/PageNotFound.jsx
+++ client/src/pages/notfound/PageNotFound.jsx
@@ -1,5 +1,5 @@
 import { useContext } from "react";
-import { Container } from "@mui/material";
+import Container from "@mui/material/Container";
 import { NavLink, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../helper/AuthContext";
 
@@ -8,18 +8,17 @@ const PageNotFound = () => {
   const { setAuthState } = useContext(AuthContext);
 
   const handleRedirect = async () => {
-    localStorage.removeItem(`accessToken`);
-    setAuthState({ id: 0, name: ``, username: ``, type: `` });
+    localStorage.removeItem("accessToken");
+    setAuthState({ id: 0, name: "", username: "", type: "" });
     return navigate("/");
   };
+
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "90vh", width: "100vw" }}>
       <Container maxWidth="sm">
-        {/* <div style={{ textAlign: "center", marginTop: "50px" }}> */}
         <h1>404</h1>
         <h2>Page Not Found</h2>
         <p>Sorry, the page you are looking for does not exist.</p>
-
         <NavLink to="/" onClick={handleRedirect} style={{ textDecoration: "none" }}>
           Go back to login page
         </NavLink>
