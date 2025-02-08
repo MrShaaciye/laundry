a50f02c676f0f151a979f0fc7ac2ed2d681d0df2
MrShaaciye <shaaciye122@gmail.com>
2025-02-08 10:33:49 +0300

    commit

--- client/src/components/formsUI/TextFieldWrapper.jsx
+++ client/src/components/formsUI/TextFieldWrapper.jsx
@@ -1,9 +1,9 @@
 "use client";
 import { TextField } from "@mui/material";
 
-const TextFieldWrapper = ({ field, form, ...props }) => {
+const TextFieldWrapper = ({ field = {}, form = {}, ...props }) => {
   const { name, value, onChange, onBlur } = field;
-  const { errors, touched } = form;
+  const { errors = {}, touched = {} } = form;
   const fieldError = touched[name] && errors[name];
 
   return <TextField required variant="outlined" color="primary" size="small" sx={{ minWidth: "100%" }} autoComplete="off" fullWidth {...props} name={name} value={value || ""} onChange={onChange} onBlur={onBlur} error={Boolean(fieldError)} helperText={fieldError} />;
